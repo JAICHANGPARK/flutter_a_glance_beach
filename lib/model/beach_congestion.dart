557515989183f7011fd2f3125e89977ccbbd3571
Dreamwalker <aristojeff@gmail.com>
2020-07-20 21:54:51 +0900

    Create beach_congestion.dart

---
+++ lib/model/beach_congestion.dart
@@ -0,0 +1,390 @@
+class BeachCongestion {
+  Beach0 beach0;
+  Beach0 beach1;
+  Beach0 beach2;
+  Beach0 beach3;
+  Beach0 beach4;
+  Beach0 beach5;
+  Beach0 beach6;
+  Beach0 beach7;
+  Beach0 beach8;
+  Beach0 beach9;
+  Beach0 beach10;
+  Beach0 beach11;
+  Beach0 beach12;
+  Beach0 beach13;
+  Beach0 beach14;
+  Beach0 beach15;
+  Beach0 beach16;
+  Beach0 beach17;
+  Beach0 beach18;
+  Beach0 beach19;
+  Beach0 beach20;
+  Beach0 beach21;
+  Beach0 beach22;
+  Beach0 beach23;
+  Beach0 beach24;
+  Beach0 beach25;
+  Beach0 beach26;
+  Beach0 beach27;
+  Beach0 beach28;
+  Beach0 beach29;
+  Beach0 beach30;
+  Beach0 beach31;
+  Beach0 beach32;
+  Beach0 beach33;
+  Beach0 beach34;
+  Beach0 beach35;
+  Beach0 beach36;
+  Beach0 beach37;
+  Beach0 beach38;
+  Beach0 beach39;
+  Beach0 beach40;
+  Beach0 beach41;
+  Beach0 beach42;
+  Beach0 beach43;
+  Beach0 beach44;
+  Beach0 beach45;
+  Beach0 beach46;
+  Beach0 beach47;
+  Beach0 beach48;
+  Beach0 beach49;
+
+  BeachCongestion(
+      {this.beach0,
+        this.beach1,
+        this.beach2,
+        this.beach3,
+        this.beach4,
+        this.beach5,
+        this.beach6,
+        this.beach7,
+        this.beach8,
+        this.beach9,
+        this.beach10,
+        this.beach11,
+        this.beach12,
+        this.beach13,
+        this.beach14,
+        this.beach15,
+        this.beach16,
+        this.beach17,
+        this.beach18,
+        this.beach19,
+        this.beach20,
+        this.beach21,
+        this.beach22,
+        this.beach23,
+        this.beach24,
+        this.beach25,
+        this.beach26,
+        this.beach27,
+        this.beach28,
+        this.beach29,
+        this.beach30,
+        this.beach31,
+        this.beach32,
+        this.beach33,
+        this.beach34,
+        this.beach35,
+        this.beach36,
+        this.beach37,
+        this.beach38,
+        this.beach39,
+        this.beach40,
+        this.beach41,
+        this.beach42,
+        this.beach43,
+        this.beach44,
+        this.beach45,
+        this.beach46,
+        this.beach47,
+        this.beach48,
+        this.beach49});
+
+  BeachCongestion.fromJson(Map<String, dynamic> json) {
+    beach0 =
+    json['Beach0'] != null ? new Beach0.fromJson(json['Beach0']) : null;
+    beach1 =
+    json['Beach1'] != null ? new Beach0.fromJson(json['Beach1']) : null;
+    beach2 =
+    json['Beach2'] != null ? new Beach0.fromJson(json['Beach2']) : null;
+    beach3 =
+    json['Beach3'] != null ? new Beach0.fromJson(json['Beach3']) : null;
+    beach4 =
+    json['Beach4'] != null ? new Beach0.fromJson(json['Beach4']) : null;
+    beach5 =
+    json['Beach5'] != null ? new Beach0.fromJson(json['Beach5']) : null;
+    beach6 =
+    json['Beach6'] != null ? new Beach0.fromJson(json['Beach6']) : null;
+    beach7 =
+    json['Beach7'] != null ? new Beach0.fromJson(json['Beach7']) : null;
+    beach8 =
+    json['Beach8'] != null ? new Beach0.fromJson(json['Beach8']) : null;
+    beach9 =
+    json['Beach9'] != null ? new Beach0.fromJson(json['Beach9']) : null;
+    beach10 =
+    json['Beach10'] != null ? new Beach0.fromJson(json['Beach10']) : null;
+    beach11 =
+    json['Beach11'] != null ? new Beach0.fromJson(json['Beach11']) : null;
+    beach12 =
+    json['Beach12'] != null ? new Beach0.fromJson(json['Beach12']) : null;
+    beach13 =
+    json['Beach13'] != null ? new Beach0.fromJson(json['Beach13']) : null;
+    beach14 =
+    json['Beach14'] != null ? new Beach0.fromJson(json['Beach14']) : null;
+    beach15 =
+    json['Beach15'] != null ? new Beach0.fromJson(json['Beach15']) : null;
+    beach16 =
+    json['Beach16'] != null ? new Beach0.fromJson(json['Beach16']) : null;
+    beach17 =
+    json['Beach17'] != null ? new Beach0.fromJson(json['Beach17']) : null;
+    beach18 =
+    json['Beach18'] != null ? new Beach0.fromJson(json['Beach18']) : null;
+    beach19 =
+    json['Beach19'] != null ? new Beach0.fromJson(json['Beach19']) : null;
+    beach20 =
+    json['Beach20'] != null ? new Beach0.fromJson(json['Beach20']) : null;
+    beach21 =
+    json['Beach21'] != null ? new Beach0.fromJson(json['Beach21']) : null;
+    beach22 =
+    json['Beach22'] != null ? new Beach0.fromJson(json['Beach22']) : null;
+    beach23 =
+    json['Beach23'] != null ? new Beach0.fromJson(json['Beach23']) : null;
+    beach24 =
+    json['Beach24'] != null ? new Beach0.fromJson(json['Beach24']) : null;
+    beach25 =
+    json['Beach25'] != null ? new Beach0.fromJson(json['Beach25']) : null;
+    beach26 =
+    json['Beach26'] != null ? new Beach0.fromJson(json['Beach26']) : null;
+    beach27 =
+    json['Beach27'] != null ? new Beach0.fromJson(json['Beach27']) : null;
+    beach28 =
+    json['Beach28'] != null ? new Beach0.fromJson(json['Beach28']) : null;
+    beach29 =
+    json['Beach29'] != null ? new Beach0.fromJson(json['Beach29']) : null;
+    beach30 =
+    json['Beach30'] != null ? new Beach0.fromJson(json['Beach30']) : null;
+    beach31 =
+    json['Beach31'] != null ? new Beach0.fromJson(json['Beach31']) : null;
+    beach32 =
+    json['Beach32'] != null ? new Beach0.fromJson(json['Beach32']) : null;
+    beach33 =
+    json['Beach33'] != null ? new Beach0.fromJson(json['Beach33']) : null;
+    beach34 =
+    json['Beach34'] != null ? new Beach0.fromJson(json['Beach34']) : null;
+    beach35 =
+    json['Beach35'] != null ? new Beach0.fromJson(json['Beach35']) : null;
+    beach36 =
+    json['Beach36'] != null ? new Beach0.fromJson(json['Beach36']) : null;
+    beach37 =
+    json['Beach37'] != null ? new Beach0.fromJson(json['Beach37']) : null;
+    beach38 =
+    json['Beach38'] != null ? new Beach0.fromJson(json['Beach38']) : null;
+    beach39 =
+    json['Beach39'] != null ? new Beach0.fromJson(json['Beach39']) : null;
+    beach40 =
+    json['Beach40'] != null ? new Beach0.fromJson(json['Beach40']) : null;
+    beach41 =
+    json['Beach41'] != null ? new Beach0.fromJson(json['Beach41']) : null;
+    beach42 =
+    json['Beach42'] != null ? new Beach0.fromJson(json['Beach42']) : null;
+    beach43 =
+    json['Beach43'] != null ? new Beach0.fromJson(json['Beach43']) : null;
+    beach44 =
+    json['Beach44'] != null ? new Beach0.fromJson(json['Beach44']) : null;
+    beach45 =
+    json['Beach45'] != null ? new Beach0.fromJson(json['Beach45']) : null;
+    beach46 =
+    json['Beach46'] != null ? new Beach0.fromJson(json['Beach46']) : null;
+    beach47 =
+    json['Beach47'] != null ? new Beach0.fromJson(json['Beach47']) : null;
+    beach48 =
+    json['Beach48'] != null ? new Beach0.fromJson(json['Beach48']) : null;
+    beach49 =
+    json['Beach49'] != null ? new Beach0.fromJson(json['Beach49']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.beach0 != null) {
+      data['Beach0'] = this.beach0.toJson();
+    }
+    if (this.beach1 != null) {
+      data['Beach1'] = this.beach1.toJson();
+    }
+    if (this.beach2 != null) {
+      data['Beach2'] = this.beach2.toJson();
+    }
+    if (this.beach3 != null) {
+      data['Beach3'] = this.beach3.toJson();
+    }
+    if (this.beach4 != null) {
+      data['Beach4'] = this.beach4.toJson();
+    }
+    if (this.beach5 != null) {
+      data['Beach5'] = this.beach5.toJson();
+    }
+    if (this.beach6 != null) {
+      data['Beach6'] = this.beach6.toJson();
+    }
+    if (this.beach7 != null) {
+      data['Beach7'] = this.beach7.toJson();
+    }
+    if (this.beach8 != null) {
+      data['Beach8'] = this.beach8.toJson();
+    }
+    if (this.beach9 != null) {
+      data['Beach9'] = this.beach9.toJson();
+    }
+    if (this.beach10 != null) {
+      data['Beach10'] = this.beach10.toJson();
+    }
+    if (this.beach11 != null) {
+      data['Beach11'] = this.beach11.toJson();
+    }
+    if (this.beach12 != null) {
+      data['Beach12'] = this.beach12.toJson();
+    }
+    if (this.beach13 != null) {
+      data['Beach13'] = this.beach13.toJson();
+    }
+    if (this.beach14 != null) {
+      data['Beach14'] = this.beach14.toJson();
+    }
+    if (this.beach15 != null) {
+      data['Beach15'] = this.beach15.toJson();
+    }
+    if (this.beach16 != null) {
+      data['Beach16'] = this.beach16.toJson();
+    }
+    if (this.beach17 != null) {
+      data['Beach17'] = this.beach17.toJson();
+    }
+    if (this.beach18 != null) {
+      data['Beach18'] = this.beach18.toJson();
+    }
+    if (this.beach19 != null) {
+      data['Beach19'] = this.beach19.toJson();
+    }
+    if (this.beach20 != null) {
+      data['Beach20'] = this.beach20.toJson();
+    }
+    if (this.beach21 != null) {
+      data['Beach21'] = this.beach21.toJson();
+    }
+    if (this.beach22 != null) {
+      data['Beach22'] = this.beach22.toJson();
+    }
+    if (this.beach23 != null) {
+      data['Beach23'] = this.beach23.toJson();
+    }
+    if (this.beach24 != null) {
+      data['Beach24'] = this.beach24.toJson();
+    }
+    if (this.beach25 != null) {
+      data['Beach25'] = this.beach25.toJson();
+    }
+    if (this.beach26 != null) {
+      data['Beach26'] = this.beach26.toJson();
+    }
+    if (this.beach27 != null) {
+      data['Beach27'] = this.beach27.toJson();
+    }
+    if (this.beach28 != null) {
+      data['Beach28'] = this.beach28.toJson();
+    }
+    if (this.beach29 != null) {
+      data['Beach29'] = this.beach29.toJson();
+    }
+    if (this.beach30 != null) {
+      data['Beach30'] = this.beach30.toJson();
+    }
+    if (this.beach31 != null) {
+      data['Beach31'] = this.beach31.toJson();
+    }
+    if (this.beach32 != null) {
+      data['Beach32'] = this.beach32.toJson();
+    }
+    if (this.beach33 != null) {
+      data['Beach33'] = this.beach33.toJson();
+    }
+    if (this.beach34 != null) {
+      data['Beach34'] = this.beach34.toJson();
+    }
+    if (this.beach35 != null) {
+      data['Beach35'] = this.beach35.toJson();
+    }
+    if (this.beach36 != null) {
+      data['Beach36'] = this.beach36.toJson();
+    }
+    if (this.beach37 != null) {
+      data['Beach37'] = this.beach37.toJson();
+    }
+    if (this.beach38 != null) {
+      data['Beach38'] = this.beach38.toJson();
+    }
+    if (this.beach39 != null) {
+      data['Beach39'] = this.beach39.toJson();
+    }
+    if (this.beach40 != null) {
+      data['Beach40'] = this.beach40.toJson();
+    }
+    if (this.beach41 != null) {
+      data['Beach41'] = this.beach41.toJson();
+    }
+    if (this.beach42 != null) {
+      data['Beach42'] = this.beach42.toJson();
+    }
+    if (this.beach43 != null) {
+      data['Beach43'] = this.beach43.toJson();
+    }
+    if (this.beach44 != null) {
+      data['Beach44'] = this.beach44.toJson();
+    }
+    if (this.beach45 != null) {
+      data['Beach45'] = this.beach45.toJson();
+    }
+    if (this.beach46 != null) {
+      data['Beach46'] = this.beach46.toJson();
+    }
+    if (this.beach47 != null) {
+      data['Beach47'] = this.beach47.toJson();
+    }
+    if (this.beach48 != null) {
+      data['Beach48'] = this.beach48.toJson();
+    }
+    if (this.beach49 != null) {
+      data['Beach49'] = this.beach49.toJson();
+    }
+    return data;
+  }
+}
+
+class Beach0 {
+  String etlDt;
+  int seqId;
+  String poiNm;
+  int uniqPop;
+  String congestion;
+
+  Beach0({this.etlDt, this.seqId, this.poiNm, this.uniqPop, this.congestion});
+
+  Beach0.fromJson(Map<String, dynamic> json) {
+    etlDt = json['etlDt'];
+    seqId = json['seqId'];
+    poiNm = json['poiNm'];
+    uniqPop = json['uniqPop'];
+    congestion = json['congestion'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['etlDt'] = this.etlDt;
+    data['seqId'] = this.seqId;
+    data['poiNm'] = this.poiNm;
+    data['uniqPop'] = this.uniqPop;
+    data['congestion'] = this.congestion;
+    return data;
+  }
+}
