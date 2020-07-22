086278b290195e00a16e17b903827a99f6d3c224
Dreamwalker <aristojeff@gmail.com>
2020-07-22 21:03:50 +0900

    Create intro_page.dart

---
+++ lib/ui/intro_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), (){
+      Navigator.of(context).pushNamedAndRemoveUntil("/home", (route) => false);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage("https://cdn.pixabay.com/photo/2015/07/05/13/44/beach-832346_960_720.jpg"),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 64,
+            right: 0,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("모해", style: TextStyle(
+                      fontSize: 120,
+                      color: Colors.white
+                  ),),
+                  Text("모두의 해수욕장", style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.white
+                  ),),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
